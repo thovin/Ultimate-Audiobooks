782df30631e7e35985495a2f0ce3fa865015c2c2
TJordan <Tjordan102@gmail.com>
2024-04-01 21:00:47 -0500

    complete rough draft single level batch

--- Main/FileManagement.py
+++ Main/FileManagement.py
@@ -2,74 +2,95 @@ from Settings import settings
 from pathlib import Path
 from itertools import islice
 import mutagen
-from mutagen import id3, easyid3, mp3, mp4
+#from mutagen import id3, easyid3, mp3, mp4
 from Util import *
 import xml.etree.ElementTree as ET
-
-
-def singleLevelBatch():
-    infolder = Path(settings.input)
-    files = list(islice(infolder.glob("*.m4*"), settings.batch))    #.m4a, .m4b
-
-    if len(files) < settings.batch:
-        files.append(list(islice(infolder.glob("*.mp*"), settings.batch - len(files)))) #.mp3, .mp4
-
-    # if len(files) < settings.batch:
-    #     files.append(list(islice(infolder.glob("*.flac"), settings.batch - len(files))))
-
-    # if len(files) < settings.batch:
-    #     files.append(list(islice(infolder.glob("*.wma"), settings.batch - len(files))))
-
-    for file in files:
-        track = mutagen.File(file, easy=True)
-        if settings.fetch:
-            #existing OPF is ignored in single level batch
-            md = fetchMetadata(file, track)
-            if settings.create:
-                opf = createOpf(md)
-
-            if settings.clean:
-                #TODO overwrite file md
-                pass
-
-        if settings.convert and :#TODO file not m4b
-            file = convertToM4B(file)
-
-        if settings.rename:
-            #TODO
-            pass
-
-        if settings.move:
-            #TODO
-            pass
-        else:
-            #TODO same but copy. In case of conversion, delete original?
-            
+import subprocess
+import os
+import shutil
 
 
 
-
-
-def recursivelyFetchBatch():
-
-
-def recursivelyCombineBatch():   #TODO single recurse func checking flags?
-
-
-
-def recursivelyPreserveBatch():
 
 
 
 def combine(folder):
+    return
 
 
 
-def convertToM4B(file):
-    #delete original file?
+def convertToM4B(file, type):
+    cmd = ['ffmpeg',
+           '-i', file,  #input file
+           '-codec:a', 'aac', #codec, audio
+           '-b:a', '64k', #bitrate, audio
+           '-vn',   #disable video
+           '-f', 'mp4', #force output format
+           file.name]    #output file. do I have to cast to Path first?
+    
+    if type == '.mp3':
+        try:
+            subprocess.run(cmd, check=True)
+            newFile = file.with_suffix('.m4b')
+            file.unlink()   #delete original file
+            file = newFile
+        except subprocess.CalledProcessError as e:
+            pass    #ERROR
+
+    elif type == '.mp4':
+        newName = file.with_suffix('.m4b')
+        file.rename(newName)
 
 
-def cleanMetadata(file, md):
+
+def cleanMetadata(file, type, md):
+    if type == '.mp3':
+        data = mutagen.ID3(file)    #can put in try-except if needed
+
+        data.delete()
+        data.add(mutagen.TIT2(encoding = 3, text = md.title))
+        data.add(mutagen.TPE1(encoding = 3, text = md.narrator))
+        data.add(mutagen.TALB(encoding = 3, text = md.series))
+        data.add(mutagen.TYER(encoding = 3, text = md.publishYear))
+        data.add(mutagen.TPOS(encoding = 3, text = md.volumeNumber))
+        data.add(mutagen.TCOM(encoding = 3, text = md.author))
+        # data.add(mutagen.TCON(encoding = 3, text = md.md.genres[0]))  #can list multiples by separating with /, //, or ;
+        data.add(mutagen.TPUB(encoding = 3, text = md.publisher))
+        data.add(mutagen.TXXX(encoding = 3, desc='description', text = md.summary))
+        data.add(mutagen.TXXX(encoding = 3, desc='subtitle', text = md.subtitle))
+        data.add(mutagen.TXXX(encoding = 3, desc='isbn', text = md.isbn))
+        data.add(mutagen.TXXX(encoding = 3, desc='asin', text = md.asin))
+        data.add(mutagen.TXXX(encoding = 3, desc='publisher', text = md.publisher))
+
+
+        # track["TIT2"] = mutagen.TIT2(encoding = 3, text = md.title)
+        # track["TPE1"] = mutagen.TPE1(encoding = 3, text = md.narrator)
+        # track["TALB"] = mutagen.TALB(encoding = 3, text = md.series)
+        # track["TYER"] = mutagen.TYER(encoding = 3, text = md.publishYear)
+        # track["TPOS"] = mutagen.TPOS(encoding = 3, text = md.volumeNumber)
+        # track["TCOM"] = mutagen.TCOM(encoding = 3, text = md.author)
+        # # track["TCON"] = mutagen.TCON(encoding = 3, text = md.genres[0])
+        # track["TPUB"] = mutagen.TPUB(encoding = 3, text = md.publisher)
+        # trackhow
+
+    elif type == '.mp4' or type == '.m4b':  #TODO are the custom fields the best way to do it?
+        track = mutagen.MP4(file)
+
+        track['\xa9nam'] = md.title
+        # track['\xa9gen'] = md.genres[0]
+        track['\xa9day'] = md.publishYear
+        track['trkn'] = md.volumeNumber
+        track['\xa9aut'] = md.author
+        track['\xa9des'] = md.summary
+        track['\xa9nrt'] = md.narrator
+        track['----:com.thovin.isbn'] = md.isbn
+        track['----:com.thovin.asin'] = md.asin
+        track['----:com.thovin.series'] = md.series
+
+    else:   #ERROR
+        return
+
+    track.save()
 
 
 
@@ -118,3 +139,53 @@ def createOpf(md):
         tree.write(outFile, xml_declaration=True, encoding="utf-8", method="xml")
 
 
+def singleLevelBatch():
+    infolder = Path(settings.input)
+    files = list(islice(infolder.glob("*.m4*"), settings.batch))    #.m4a, .m4b
+
+    if len(files) < settings.batch:
+        files.append(list(islice(infolder.glob("*.mp*"), settings.batch - len(files)))) #.mp3, .mp4
+
+    # if len(files) < settings.batch:
+    #     files.append(list(islice(infolder.glob("*.flac"), settings.batch - len(files))))
+
+    # if len(files) < settings.batch:
+    #     files.append(list(islice(infolder.glob("*.wma"), settings.batch - len(files))))
+
+    for file in files:
+        track = mutagen.File(file, easy=True)
+        type = Path(file).suffix.lower()
+
+        if settings.fetch:
+            #existing OPF is ignored in single level batch
+            md = fetchMetadata(file, track)
+            if settings.create:
+                opf = createOpf(md)
+
+            if settings.clean and not settings.convert:
+                cleanMetadata(file, type, md)
+
+        if settings.convert and type != '.m4b':
+            file = convertToM4B(file, type)
+
+        if settings.rename:
+            #TODO
+            pass
+
+        if settings.move:
+            # file.rename(settings.output + file.name)
+            shutil.move(file, settings.output + file.name)
+        else:
+            shutil.copy(file, settings.output + file.name)
+            
+
+def recursivelyFetchBatch():
+    return
+
+
+def recursivelyCombineBatch():   #TODO single recurse func checking flags?
+    return
+
+
+def recursivelyPreserveBatch():
+    return
