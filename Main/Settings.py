1c0271a2228bf7aa4bc4a8053e8c40761ffd346d
TimJ <t@jrdn.me>
2025-07-15 20:04:35 -0500

    add right single quotation mark to sanitize

--- Main/Settings.py
+++ Main/Settings.py
@@ -70,7 +70,7 @@ class Settings:
                 sys.exit()
 
     def checkFolders(self): #TODO this is really only a problem when using ffmpeg, I think. Cut this check and/or only check when going to use it and only check in those functions?
-        specials = re.compile(r'[<>"|?*\x00-\x1F]')
+        specials = re.compile(r'[<>"|?’*\x00-\x1F]')
         inDirs = self.input.split(os.sep)
         outDirs = self.output.split(os.sep)
 
